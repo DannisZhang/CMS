3b20f9d2da4f2706ea213ba91d053071bc5c0416
deng.zhang <deng.zhang@tcl.com>
2015-05-06 21:32:33 +0800

    提交代码

--- cms-service/src/main/java/org/dannis/cms/MobilePhoneExcelParser.java
+++ cms-service/src/main/java/org/dannis/cms/MobilePhoneExcelParser.java
@@ -4,7 +4,7 @@ import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.dannis.cms.model.PhoneNumber;
+import org.dannis.cms.model.MobilePhone;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,15 +13,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 电话号码Excel解析器
+ * 手机号码Excel解析器
  *
  * @author deng.zhang
  * @version 1.0.0
  * @date 2015-05-04 21:56
  */
 public class MobilePhoneExcelParser {
-    public static List<PhoneNumber> parseExcel(String filePath) throws IOException {
-        List<PhoneNumber> phoneNumbers = new ArrayList<PhoneNumber>();
+    public static List<MobilePhone> parseExcel(String filePath) throws IOException {
+        List<MobilePhone> mobilePhones = new ArrayList<MobilePhone>();
         InputStream inputStream = new FileInputStream(filePath);
         HSSFWorkbook workbook = new HSSFWorkbook(inputStream);
         //Read the sheets of workbook
@@ -34,34 +34,64 @@ public class MobilePhoneExcelParser {
             for (int rowIndex = 1;rowIndex <= sheet.getLastRowNum();rowIndex++) {
                 HSSFRow row = sheet.getRow(rowIndex);
                 if (row != null) {
-                    PhoneNumber phoneNumber = new PhoneNumber();
-                    phoneNumber.setNumber(getValue(row.getCell(0)));
-                    phoneNumber.setType(getValue(row.getCell(1)));
-                    phoneNumber.setOperator(getValue(row.getCell(2)));
-                    phoneNumber.setAttribution(getValue(row.getCell(3)));
-                    phoneNumber.setWholesalePrice(row.getCell(4).getNumericCellValue());
-                    phoneNumber.setFloorPrice(row.getCell(5).getNumericCellValue());
-                    phoneNumber.setDescription(row.getCell(6).getStringCellValue());
+                    String mobilePhoneNumber = getCellData(row.getCell(0));
+                    if (mobilePhoneNumber != null && !"".equals(mobilePhoneNumber.trim())) {
+                        MobilePhone mobilePhone = new MobilePhone();
+                        mobilePhone.setNumber(getCellData(row.getCell(0)));
+                        mobilePhone.setOperator(getCellData(row.getCell(1)));
+                        mobilePhone.setAttribution(getCellData(row.getCell(2)));
+                        String wholesalePriceStr = getCellData(row.getCell(3));
+                        if (wholesalePriceStr != null && !"".equals(wholesalePriceStr.trim())) {
+                            mobilePhone.setWholesalePrice(Double.parseDouble(wholesalePriceStr));
+                        } else {
+                            mobilePhone.setWholesalePrice(0.00);
+                        }
+                        String floorPriceStr = getCellData(row.getCell(4));
+                        if (floorPriceStr != null && !"".equals(floorPriceStr.trim())) {
+                            mobilePhone.setFloorPrice(Double.parseDouble(floorPriceStr));
+                        } else {
+                            mobilePhone.setFloorPrice(0.00);
+                        }
+                        String balanceStr = getCellData(row.getCell(5));
+                        if (balanceStr != null && !"".equals(balanceStr.trim())) {
+                            mobilePhone.setBalance(Double.parseDouble(balanceStr));
+                        } else {
+                            mobilePhone.setBalance(0.00);
+                        }
+                        mobilePhone.setDescription(getCellData(row.getCell(6)));
 
-                    phoneNumbers.add(phoneNumber);
+                        mobilePhones.add(mobilePhone);
+                    }
                 }
             }
         }
-        return phoneNumbers;
+        return mobilePhones;
     }
 
-    private static String getValue(HSSFCell cell) {
-        if (HSSFCell.CELL_TYPE_BOOLEAN == cell.getCellType()) {
-            return String.valueOf(cell.getBooleanCellValue());
-        } else if (HSSFCell.CELL_TYPE_NUMERIC == cell.getCellType()) {
-            return String.valueOf(cell.getNumericCellValue());
-        } else {
-            return String.valueOf(cell.getStringCellValue());
+    /**
+     * 获取单元格数据（String）
+     *
+     * @param cell 单元格
+     * @return 单元格数据（String）
+     */
+    private static String getCellData(HSSFCell cell) {
+        if (cell == null) {
+            return null;
         }
+        String data;
+        if (HSSFCell.CELL_TYPE_BOOLEAN == cell.getCellType()) {
+            data = String.valueOf(cell.getBooleanCellValue());
+        } else if (HSSFCell.CELL_TYPE_NUMERIC == cell.getCellType()) {
+            data =  String.valueOf(cell.getNumericCellValue());
+        } else {
+            data = cell.getStringCellValue();
+        }
+
+        return data;
     }
 
     public static void main(String[] args) throws IOException {
-        List<PhoneNumber> phoneNumbers = parseExcel("E:\\phoneNumber.xls");
-        System.out.println(phoneNumbers);
+        List<MobilePhone> mobilePhones = parseExcel("E:\\模板-批量导入手机号码.xls");
+        System.out.println(mobilePhones);
     }
 }
